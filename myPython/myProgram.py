16791b0a4df0528b6b4353b943fc9fca6adaf83a
N-Lambin <n.lambin@students.ephec.be>
2018-05-16 15:28:51 +0200

    gh

--- myPython/myProgram.py
+++ myPython/myProgram.py
@@ -1,5 +1,6 @@
 import myLib.myDataExtractor as mde
 import glob
+import os
 
 csvRecordsFile = '.\\csv\\csvRecords\\'
 csvRecordsDirList = glob.glob(csvRecordsFile+'*')
@@ -11,13 +12,20 @@ for i in range(0, len(csvRecordsDirList)):
         csvFileName = csvRecordsFileList[l].split('\\')[4].split('.')[0]
         mde.csvCleaner(currentDirName, csvFileName)
 
-""" newFileDir = '.\\csv\\'
-    newFileName = 'csvMLData'
+newFileDir = '.\\csv\\'
+newFileName = 'csvMLData'
+
+""" channelList = ['AF3', 'F7', 'F3', 'F4', 'F8', 'AF4']
+frequencyList = ['16Hz', '32Hz', '48Hz', '64Hz'] """
+
+csvCleanData = '.\\csv\\csvCleanData\\'
+csvCleanDataList = glob.glob(csvCleanData+'*')
     
-    if os.path.exists(newFileDir + newFileName + '.csv'):
-        os.remove(newFileDir + newFileName + '.csv')
+if os.path.exists(newFileDir + newFileName + '.csv'):
+    os.remove(newFileDir + newFileName + '.csv')
     
-    with open(newFileDir + newFileName + '.csv', 'a+') as csvfile: """
+with open(newFileDir + newFileName + '.csv', 'a+') as csvfile:
+    
 
 hope = mde.csvToPeriodogram('neutral', 'neutral1')
 print(hope)
