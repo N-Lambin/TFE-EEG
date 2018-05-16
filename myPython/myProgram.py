8691217397b3dc9556f08e37a30d4a27921cd7f9
N-Lambin <n.lambin@students.ephec.be>
2018-05-16 11:06:12 +0200

    azerty

--- myPython/myProgram.py
+++ myPython/myProgram.py
@@ -1,3 +1,14 @@
 import myLib.myDataExtractor as mde
+import glob
 
-mde.csvCleaner('neutral', 'neutral1')
+csvRecordsFile = '.\\csv\\csvRecords\\'
+csvRecordsDirList = glob.glob(csvRecordsFile+'*')
+
+for i in range(0, len(csvRecordsDirList)):
+    currentDirName = csvRecordsDirList[i].split('\\')[3]
+    csvRecordsFileList = glob.glob(csvRecordsDirList[i]+'\\*.csv')
+    #if len(csvRecordsFileList) != 0:
+    for l in range(0, len(csvRecordsFileList)):
+        print(csvRecordsFileList[l].split('\\')[4].split('.')[0])
+
+#mde.csvCleaner('neutral', 'neutral1')
