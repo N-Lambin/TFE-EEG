70a7cb3cb51922dcbf77ad49daaef0b8628c1cb7
N-Lambin <n.lambin@students.ephec.be>
2018-06-02 14:14:57 +0200

    gofhewe

--- Python/myLib/mySupervisedLearning.py
+++ Python/myLib/mySupervisedLearning.py
@@ -5,7 +5,7 @@ from sklearn.tree import DecisionTreeClassifier
 def trainingDecisionTree():
     names = ['AF3-2Hz', 'AF3-4Hz', 'F3-2Hz', 'F3-4Hz', 'F4-2Hz', 'F4-4Hz', 'F8-2Hz', 'F8-4Hz', 'AF4-2Hz', 'AF4-4Hz', 'class']
     dataset = pandas.read_csv('.\\csv\\csvMLData.csv', names=names)
-    
+
     array = dataset.values
     X = array[:,0:10]
     Y = array[:,10]
@@ -14,3 +14,7 @@ def trainingDecisionTree():
     model.fit(X, Y)
 
     return model
+
+
+
+    
